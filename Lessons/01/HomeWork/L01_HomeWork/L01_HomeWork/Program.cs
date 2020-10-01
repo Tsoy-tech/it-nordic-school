0a08ae76f5f9e9557da20e121d57535d45e5af6a
Tsoy-tech <tsoy230596@gmail.com>
2020-10-01 18:35:29 +0300

    Home work of the 1st lesson added

---
+++ Lessons/01/HomeWork/L01_HomeWork/L01_HomeWork/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace L01_HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Здравствуйте, {name}!");
+            Thread.Sleep(5000);
+            Console.WriteLine($"Всего доброго, {name}!");
+            Console.ReadKey();
+        }
+    }
+}
+
