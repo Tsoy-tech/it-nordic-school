3a031ae4641d069f48be30d720344f31cf7c9e9f
Tsoy-tech <tsoy230596@gmail.com>
2020-10-13 14:43:06 +0300

    rework spaces between blocks

--- Lessons/04/L04_HomeWork/L04_HomeWork/Program.cs
+++ Lessons/04/L04_HomeWork/L04_HomeWork/Program.cs
@@ -1,75 +1,62 @@
 ﻿using System;
-using System.Drawing;
-using System.Dynamic;
 using System.Globalization;
-using System.Security.Authentication;
 
 namespace L04_HomeWork
 {
 	class Program
 	{ 
-		enum JuiceСontainers
-		{
-			One = 1,
-			Five = 5,
-			Twenty = 20
-		};
 		static void Main(string[] args)
-		{				
-			int howManyTwenty;
-			int howManyFive;
-			int howManyOne;
+		{
+			int howManyLarge;
+			int howManyMedium;
+			int howManySmall;
 			int remainderAfterTwenty;
 			int remainderAfterFive;
-
 			float volume;
-
 			string inputeVolume;
+			const int smallContainer = 1;
+			const int mediumContainer = 5;
+			const int largeContainer = 20;
 
 			Console.ForegroundColor = ConsoleColor.DarkYellow;
 			Console.WriteLine("Какой объем сока (в литрах) требуется упаковать?\n" + "Объем:");
-
 			Console.ForegroundColor = ConsoleColor.DarkGray;
 			Console.WriteLine($"Примечание: в качестве десятичного разделителя используйте " +
 				$"\'{CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator}\'\n" +
 				"---------------------------------------------------------------");
-
 			Console.ResetColor();
 			Console.SetCursorPosition(6, 1);
 			inputeVolume = Console.ReadLine();
 			volume = float.Parse(inputeVolume);
 
-			howManyTwenty = (int)Math.Ceiling(volume) / (int)JuiceСontainers.Twenty;
-
-			remainderAfterTwenty = (int)Math.Ceiling(volume) % (int)JuiceСontainers.Twenty;
-			howManyFive = remainderAfterTwenty / (int)JuiceСontainers.Five;
-
-			remainderAfterFive = remainderAfterTwenty % (int)JuiceСontainers.Five;
-			howManyOne = remainderAfterFive / (int)JuiceСontainers.One;
+			howManyLarge = (int)Math.Ceiling(volume) / largeContainer;
+			remainderAfterTwenty = (int)Math.Ceiling(volume) % largeContainer;
+			howManyMedium = remainderAfterTwenty / mediumContainer;
+			remainderAfterFive = remainderAfterTwenty % mediumContainer;
+			howManySmall = remainderAfterFive / smallContainer;
 
 			Console.ForegroundColor = ConsoleColor.DarkYellow;
 			Console.SetCursorPosition(0, 4);
 			Console.WriteLine("Вам потребуются следующие контейнеры:");
-			if (howManyTwenty > 0)
+			if (howManyLarge > 0)
 			{
-				Console.WriteLine($"{((int)JuiceСontainers.Twenty).ToString()}".PadLeft(3) +
+				Console.WriteLine($"{largeContainer.ToString()}".PadLeft(3) +
 					" л: ".PadRight(1) +
-					howManyTwenty.ToString() + " шт.") ;
+					howManyLarge.ToString() + " шт.") ;
+			}
+			if (howManyMedium > 0)
+			{
+				Console.WriteLine($"{mediumContainer.ToString()}".PadLeft(3) + 
+					" л: ".PadRight(1) + 
+					howManyMedium.ToString() + " шт.");
+			}
+			if (howManySmall > 0)
+			{
+				Console.WriteLine($"{smallContainer.ToString()}".PadLeft(3) + 
+					" л: ".PadRight(1) + 
+					howManySmall.ToString() + " шт.");
 			}
 
-			if (howManyFive > 0)
-			{
-				Console.WriteLine($"{((int)JuiceСontainers.Five).ToString()}".PadLeft(3) + 
-					" л: ".PadRight(1) + 
-					howManyFive.ToString() + " шт.");
-			}
-
-			if (howManyOne > 0)
-			{
-				Console.WriteLine($"{((int)JuiceСontainers.One).ToString()}".PadLeft(3) + 
-					" л: ".PadRight(1) + 
-					howManyOne.ToString() + " шт.");
-			}
 			Console.ForegroundColor = ConsoleColor.DarkGray;
 		}
 	}
