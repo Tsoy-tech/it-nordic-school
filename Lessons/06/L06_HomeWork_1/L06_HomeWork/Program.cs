5156690f64d233fa8b9c5bb3fe8428ce15b28ba9
Tsoy-tech <tsoy230596@gmail.com>
2020-10-18 18:43:02 +0300

    add home work of the 6th lesson

---
+++ Lessons/06/L06_HomeWork_1/L06_HomeWork/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace L06_HomeWork
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = string.Empty;
+			int inputNumber = 0;
+			int evenDigits = 0;
+
+			//Input
+
+			Console.WriteLine($"Введите положительное натуральное число не более 2 миллиардов:");
+
+			do
+			{
+				try
+				{
+					input = Console.ReadLine();
+					inputNumber = int.Parse(input);
+
+					if (inputNumber <= 0)
+					{
+						Console.WriteLine("Введено неверное значение! Попробуйте ещё раз:");
+						continue;
+					}
+				}
+				catch (System.OverflowException e)
+				{
+					Console.WriteLine($"Ошибка {e.GetType()}! Попробуйте ещё раз:");
+					continue;
+				}
+				catch (System.FormatException e)
+				{
+					Console.WriteLine($"Ошибка {e.GetType()}! Попробуйте ещё раз:");
+					continue;
+				}
+
+				if (inputNumber <= 2000000000 && inputNumber >= 0)
+					break;
+			}
+			while (true);
+
+			//Calculations
+
+			foreach(char digit in inputNumber.ToString())
+			{
+				int a = int.Parse(digit.ToString());
+
+				if (a % 2 == 0)
+					evenDigits++;
+			}
+
+			//Output
+
+			Console.WriteLine($"В числе {inputNumber} содержится следующее количество четных цифр: {evenDigits}.");
+		}
+	}
+}
