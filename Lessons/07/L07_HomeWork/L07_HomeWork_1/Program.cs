a8eeabda8da89ce6a1d2b157e0565991b87964e3
Tsoy-tech <tsoy230596@gmail.com>
2020-10-23 00:14:17 +0300

    add home work of the 7th lesson

---
+++ Lessons/07/L07_HomeWork/L07_HomeWork_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace L07_HomeWork_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string inputText = string.Empty;
+			string[] words = new string[] { };
+			const char startingLetter = 'А';
+			int numberOfStatingLetters = 0;
+
+			//Input
+
+			Console.WriteLine("Введите строку из нескольких слов:");
+			try
+			{
+				do
+				{
+					inputText = Console.ReadLine();
+					words = inputText.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+					if (words.Length > 1)
+						break;
+
+					Console.WriteLine("Слишком мало слов :( Попробуйте ещё раз:");
+				} while (true);
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine($"Ошибка {e.GetType()}!");
+				throw;
+			}
+
+			//Calculations
+
+			for (int i = 0; i < words.Length; i++)
+			{
+				if (words[i].StartsWith(startingLetter.ToString(), StringComparison.InvariantCultureIgnoreCase))
+				{
+					numberOfStatingLetters++;
+				}
+			}
+
+			//Output
+
+			Console.WriteLine($"Количество слов, начинающихся с буквы " +
+				$"\'{startingLetter}\': {numberOfStatingLetters}");
+			Console.WriteLine("\n" + "Нажмите любую клавишу для выхода…");
+		}
+	}
+}
---
+++ Lessons/07/L07_HomeWork/L07_HomeWork_2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace L07_HomeWork_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string inputText = string.Empty;
+			StringBuilder reverseText = new StringBuilder(50);
+
+			//Input
+
+			Console.WriteLine("Введите непустую строку:");
+			try
+			{
+				do
+				{
+					Console.ForegroundColor = ConsoleColor.DarkYellow;
+					inputText = Console.ReadLine();
+					Console.ResetColor();
+
+					if (string.IsNullOrWhiteSpace(inputText) != true)
+						break;
+
+					Console.WriteLine("Вы ввели пустую строку :( Попробуйте ещё раз:");
+				} while (true);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Ошибка {e.GetType()}!");
+				throw;
+			}
+
+			//Calculations
+
+			inputText = inputText.ToLowerInvariant();
+
+			for (int lastIndexOfLetter = inputText.Length - 1;
+				0 <= lastIndexOfLetter && lastIndexOfLetter < inputText.Length;
+				lastIndexOfLetter--)
+			{
+				reverseText.Append(new char[] { inputText[lastIndexOfLetter] });
+			}
+
+			//Output
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine(reverseText);
+			Console.ResetColor();
+			Console.WriteLine("\n" + "Нажмите любую клавишу для выхода…");
+		}
+	}
+}
