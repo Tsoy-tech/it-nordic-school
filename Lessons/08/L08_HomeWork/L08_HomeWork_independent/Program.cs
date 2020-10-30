7fab6a27320b1250396621f6c057f38b72f3d739
Tsoy-tech <tsoy230596@gmail.com>
2020-10-30 23:45:59 +0300

    add home work of the 8th lesson

---
+++ Lessons/08/L08_HomeWork/L08_HomeWork_independent/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L08_HomeWork_independent
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = string.Empty;
+			Stack<int> dishes = new Stack<int>();
+
+			Console.WriteLine("Выберите операцию(Помыть, Вытереть, Выйти): ");
+
+			do
+			{
+				//Input
+
+				input = Console.ReadLine().ToLowerInvariant().Trim();
+
+				//Calculations
+
+				switch (input)
+				{
+					case "помыть":
+						dishes.Push(1);
+						break;
+					case "вытереть":
+						if (dishes.Count > 0)
+							dishes.Pop();
+						break;
+				}
+
+				//Output
+
+				if(dishes.Count > 0)
+				Console.WriteLine("Текущее количество тарелок: " + dishes.Count);
+				else
+				{
+					Console.WriteLine("Стопка тарелок пуста!");
+				}
+				if (input == "выйти")
+					break;
+			} while (true);
+		}
+	}
+}
