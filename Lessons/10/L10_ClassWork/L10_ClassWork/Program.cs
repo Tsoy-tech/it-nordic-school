0b7bbc04f0d3698632d9867b1dfe96cde4e83850
Tsoy-tech <tsoy230596@gmail.com>
2020-11-03 06:42:29 +0300

    add class work of the 10th lesson

---
+++ Lessons/10/L10_ClassWork/L10_ClassWork/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace L10_ClassWork
+{
+	class CreditCard
+	{
+		private string _number;
+
+		public string HolderName { get; set; }
+		public string ValidBefore { get; set; }
+		public ushort Cvc { get; set; }
+
+		public string Number
+		{
+			get
+			{
+				//if (_number == null)
+				//	return null;
+
+				//string result = string.Empty;
+
+				//for(int i = 0; i < _number.Length; i++)
+				//{
+				//	if (i > 0 && i % 4 == 0)
+				//		result += " ";
+
+				//	result += _number[i];
+				//}
+
+				//return result;
+
+				return _number;
+			}
+			set
+			{
+				foreach (char ch in value)
+				{
+					if (!char.IsDigit(ch))
+					{
+						throw new Exception("Value should contain only digits.");
+					}
+				}
+				_number = value;
+			}
+		}
+
+		private bool NumberIsValid()
+		{
+			return Number != null 
+				&& Number.Length >= 12 
+				&& Number.Length <= 26;
+		}
+
+		private bool ValidBeforeIsValid()
+		{
+			return ValidBefore != null;
+		}
+
+		private bool CvcIsValid()
+		{
+			return Cvc < 1000;
+		}
+
+		public bool IsValid()
+		{
+			return NumberIsValid() 
+				&& ValidBeforeIsValid() 
+				&& CvcIsValid();
+
+			if (Number == null || Number.Length < 12 || Number.Length > 26)
+			{
+				return false;
+			}
+
+			if(ValidBefore == null)
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+
+	//Инкапсуляция - заключение в одном классе нескольких свойств и методов объекта
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CreditCard creditCard = new CreditCard();
+			creditCard.HolderName = "Anyone";
+			creditCard.Number = "11112222333344445555";
+			creditCard.ValidBefore = "12/25";
+			creditCard.Cvc = 123;
+
+			Console.WriteLine(creditCard.Number);
+			creditCard.Number += "9";
+			Console.WriteLine(creditCard.IsValid()
+				? "Credit card is VALID"
+				: "Credit card is NOT VALID");
+
+			CreditCard[] creditCards = new CreditCard[2];
+			creditCards[0] = creditCard;
+		}
+	}
+}
---
+++ Lessons/10/L10_ClassWork/L10_ClassWork_Pet/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace L10_ClassWork_Pet
+{
+	enum PetKind
+	{
+		Mouse,
+		Cat,
+		Dog
+	}
+	class Pet
+	{
+		private string _birthPlace;
+		private char _sex;
+		private byte _age;
+
+		public PetKind kind;
+		public string Name;
+
+		public char Sex 
+		{ 
+			get { return _sex; } 
+			set 
+			{
+				if (value == 'M' || value == 'm' ||
+					value == 'F' || value == 'f')
+				{ 
+					_sex = char.ToUpper(value);
+				}
+				else
+				{
+					throw new Exception("Sex should be one of two options: M/F.");
+				}
+			} 
+		}
+		public byte Age
+		{
+			get { return _age; }
+			set
+			{
+				if (value >= 150 || value <= 0)
+					throw new Exception("Age should be less or equals to 150");
+
+				_age = value;
+			}
+		}
+
+		public void SetBirthPlace(string birthPlace)
+		{
+			if (birthPlace == null)
+			{
+				throw new ArgumentNullException(nameof(birthPlace));
+			}
+
+			if(_birthPlace == null)
+				throw new Exception("Birth place can not be overriden");
+			_birthPlace = birthPlace; 
+		}
+
+		public string Description
+		{
+			get 
+			{
+				return $"{Name} is a {kind}({Sex}) of {Age} years old.";
+			}
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pet pet1 = new Pet();
+			pet1.Name = "Tom";
+			pet1.kind = PetKind.Cat;
+			pet1.Age = 5;
+			pet1.Sex = 'M';
+			pet1.SetBirthPlace("Moscow");
+
+			Pet pet2 = new Pet
+			{
+				Name = "Minnie",
+				kind = PetKind.Mouse,
+				Sex = 'F',
+				Age = 2
+
+			};
+
+			Console.WriteLine(pet1.Description);
+			Console.WriteLine(pet2.Description);
+		}
+	}
+}
