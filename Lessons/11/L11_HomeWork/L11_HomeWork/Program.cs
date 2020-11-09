791121944f7c1ee3faf1b01fddc18b1e15353a53
Tsoy-tech <tsoy230596@gmail.com>
2020-11-10 00:51:22 +0300

    add home work of the 11th lesson

---
+++ Lessons/11/L11_HomeWork/L11_HomeWork/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace L11_HomeWork
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ReminderItem breakfastReminder = new ReminderItem(
+				DateTimeOffset.Parse("2020-11-09 07:15:00"), 
+				"It's time for breakfast!");
+
+			ReminderItem lunchReminder = new ReminderItem(
+				DateTimeOffset.Parse("2020-11-20 12:00:00"),
+				"It's time for lunch!");
+
+			breakfastReminder.WriteProperties();
+			lunchReminder.WriteProperties();
+		}
+	}
+}
---
+++ Lessons/11/L11_HomeWork/L11_HomeWork/ReminderItem.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace L11_HomeWork
+{
+	class ReminderItem
+	{
+		private DateTimeOffset AlarmDate { get; set; }
+		private string AlarmMessage { get; set; }
+		private TimeSpan TimeToAlarm
+		{
+			get
+			{
+				return AlarmDate.Subtract(DateTimeOffset.Now);
+			}
+		}
+		private bool IsOutdated 
+		{
+			get 
+			{
+				return TimeToAlarm.TotalSeconds < 0;
+			}
+		}
+
+		public ReminderItem(DateTimeOffset alarmDate, string alarmMessage)
+		{
+			AlarmDate = alarmDate;
+			AlarmMessage = alarmMessage;
+		}
+
+		public void WriteProperties()
+		{
+			Console.WriteLine($"Alarm date: {AlarmDate}\n" +
+				$"Alarm message: {AlarmMessage}\n" +
+				$"Time to alarm: {TimeToAlarmLeft(TimeToAlarm)}\n" +
+				$"Is outdated: {IsOutdated}\n");
+		}
+
+		private string TimeToAlarmLeft(TimeSpan time)
+		{
+			string days = string.Empty;
+			string timeLeftFormat = string.Empty;
+
+			if (time.Days != 0)
+			{
+				days = $"{time.Days.ToString()} d.";
+			}
+			
+			timeLeftFormat = $"{days} {time:hh\\:mm\\:ss}";
+			return timeLeftFormat.Trim();
+		}
+	}
+}
