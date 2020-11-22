267798ab3afcc934d9185221758dedefa3a3458f
Tsoy-tech <tsoy230596@gmail.com>
2020-11-22 22:17:10 +0300

    add home work of the 13th lesson

---
+++ Lessons/13/L13_HomeWork/L13_HomeWork/Interface/ILogWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L13_HomeWork
+{
+	public interface ILogWriter
+	{			
+		public void LogInfo(string message);
+		public void LogWarning(string message);
+		public void LogError(string message);
+	}
+}
---
+++ Lessons/13/L13_HomeWork/L13_HomeWork/MultipleLogWriter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Collections;
+
+namespace L13_HomeWork
+{
+	public class MultipleLogWriter : AbstractLogWriter, ILogWriter
+	{
+		public List<ILogWriter> _logWriter;
+
+		public MultipleLogWriter(ILogWriter logWriter)
+		{
+			_logWriter = new List<ILogWriter>();
+			_logWriter.Add(logWriter);
+		}
+		public MultipleLogWriter(List<ILogWriter> logWriter)
+		{
+			_logWriter = logWriter;
+		}
+
+		public void WriteLog(MessageType messageType, string message)
+		{
+			foreach (object writer in _logWriter)
+			{
+				if (writer is FileLogWriter)
+				{
+					switch (messageType)
+					{
+						case MessageType.Error:
+							(writer as FileLogWriter).LogError(message);
+							break;
+						case MessageType.Info:
+							(writer as FileLogWriter).LogInfo(message);
+							break;
+						case MessageType.Warning:
+							(writer as FileLogWriter).LogInfo(message);
+							break;
+					}
+				}
+				if(writer is ConsoleLogWriter)
+				{
+					switch (messageType)
+					{
+						case MessageType.Error:
+							(writer as ConsoleLogWriter).LogError(message);
+							break;
+						case MessageType.Info:
+							(writer as ConsoleLogWriter).LogInfo(message);
+							break;
+						case MessageType.Warning:
+							(writer as ConsoleLogWriter).LogInfo(message);
+							break;
+					}
+				}
+			}
+		}
+
+		public override void LogError(string message) { }
+		public override void LogInfo(string message) { }
+		public override void LogWarning(string message) { }
+	}
+}
