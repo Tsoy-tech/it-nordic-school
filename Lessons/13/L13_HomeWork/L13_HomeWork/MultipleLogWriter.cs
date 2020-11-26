d6d0fa5be382b3fb559c2503bad72385495d69e1
Tsoy-tech <tsoy230596@gmail.com>
2020-11-27 01:46:30 +0300

    add home work of the 13th lesson

---
+++ Lessons/13/L13_HomeWork/L13_HomeWork/MultipleLogWriter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L13_HomeWork
+{
+	public class MultipleLogWriter : IDisposable, ILogWriter
+	{
+		private List<ILogWriter> _logWriters;
+
+		public MultipleLogWriter(ILogWriter logWriter)
+		{
+			_logWriters = new List<ILogWriter>();
+			_logWriters.Add(logWriter);
+		}
+		public MultipleLogWriter(List<ILogWriter> logWriters)
+		{
+			_logWriters = logWriters;
+		}
+
+		public void LogInfo(string message)
+		{
+			foreach (var writer in _logWriters)
+			{
+				if (writer is FileLogWriter)
+				{
+					(writer as FileLogWriter).LogInfo(message);
+				}
+				if (writer is ConsoleLogWriter)
+				{
+					(writer as ConsoleLogWriter).LogInfo(message);
+				}
+			}
+		}
+		public void LogWarning(string message)
+		{
+			foreach (var writer in _logWriters)
+			{
+				if (writer is FileLogWriter)
+				{
+					(writer as FileLogWriter).LogWarning(message);
+				}
+				if (writer is ConsoleLogWriter)
+				{
+					(writer as ConsoleLogWriter).LogWarning(message);
+				}
+			}
+		}
+		public void LogError(string message)
+		{
+			foreach (var writer in _logWriters)
+			{
+				if (writer is FileLogWriter)
+				{
+					(writer as FileLogWriter).LogError(message);
+				}
+				if(writer is ConsoleLogWriter)
+				{ 
+					(writer as ConsoleLogWriter).LogError(message);
+				}
+			}
+		}
+		public void Dispose()
+		{
+			foreach (object writer in _logWriters)
+			{
+				if (_logWriters is IDisposable)
+				{
+					(_logWriters as IDisposable).Dispose();
+				}
+			}
+		}
+	}
+}
---
+++ Lessons/13/L13_HomeWork/L13_HomeWork/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+
+namespace L13_HomeWork
+{
+	class Program
+	{
+		static void Main()
+		{
+			var consoleLog = new ConsoleLogWriter();
+			using var fileLog = new FileLogWriter("Logs");
+
+			List<ILogWriter> listOfLogWriters = new List<ILogWriter> 
+			{ 
+				consoleLog, 
+				fileLog
+			};
+
+			MultipleLogWriter multiple = new MultipleLogWriter(listOfLogWriters);
+			multiple.LogInfo("There is some information from Multiple Log Writer!");
+			multiple.LogError("Error!!!");
+			multiple.LogWarning("Warning!!!");
+			
+		}
+	}
+}
