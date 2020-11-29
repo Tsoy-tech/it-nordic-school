8862496c7e5bc113d30d5f0fba7b3ea43c797d3e
Tsoy-tech <tsoy230596@gmail.com>
2020-11-29 23:54:54 +0300

    add home work of the 15th lesson

---
+++ Lessons/15/L15_HomeWork/L15_HomeWork/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+
+namespace L15_HomeWork
+{
+	class Program
+	{
+		static void Main()
+		{
+			var fileLog = LogWriterFactory.GetLogWriter<FileLogWriter>("Logs");
+			var consoleLog = LogWriterFactory.GetLogWriter<ConsoleLogWriter>();
+
+			List<ILogWriter> listOfLogWriters = new List<ILogWriter>
+			{
+				fileLog,
+				consoleLog
+			};
+
+			var multiple = LogWriterFactory.GetLogWriter<MultipleLogWriter>(listOfLogWriters);
+			multiple.LogInfo("There is some information from Multiple Log Writer!");
+			multiple.LogError("Error!!!");
+			multiple.LogWarning("Warning!!!");
+		}
+	}
+}
