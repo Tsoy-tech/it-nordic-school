cf61847fde03ef6308099048f5490b56682f780e
Tsoy-tech <tsoy230596@gmail.com>
2020-12-05 14:28:39 +0300

    add class work of the 18th lesson

---
+++ CourseProject_v.1/Reminder.Storage.Core/IReminderStorage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reminder.Storage.Core
+{
+    public interface IReminderStorage
+    {
+        // Add
+        void Add(ReminderItem reminderItem);
+
+        // Update
+        void Update(ReminderItem reminderItem);
+
+        // Get
+        ReminderItem Get(Guid id);
+
+        // GetList
+        List<ReminderItem> GetList(
+            IEnumerable<ReminderItemStatus> statuses, 
+            int count = -1, // "-1" - все 
+            int startPosition = 0);
+    }
+}
---
+++ CourseProject_v.1/Reminder.TestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Reminder.Storage.InMemory;
+using Reminder.Storage.Core;
+
+namespace Reminder.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var storage = new InMemoryReminderStorage();
+            var item = new ReminderItem(DateTimeOffset.Now, "Hello World!", "TestContact");
+
+            storage.Add(item);
+            var itemFromStorage = storage.Get(item.Id);
+
+            Console.WriteLine(itemFromStorage.Message);
+        }
+    }
+}
