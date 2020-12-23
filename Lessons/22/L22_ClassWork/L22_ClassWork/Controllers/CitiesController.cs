7a76b0d1bdffbe7b5a278bb17860523d0489cfbc
Tsoy-tech <tsoy230596@gmail.com>
2020-12-23 17:00:22 +0300

    start home work №22

--- Lessons/22/L22_ClassWork/L22_ClassWork/Controllers/CitiesController.cs
+++ Lessons/22/L22_ClassWork/L22_ClassWork/Controllers/CitiesController.cs
@@ -8,6 +8,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace L22_ClassWork.Controllers
 {
+    public enum Attribute
+	{
+        Name,
+        Description,
+        NumberOfPoi
+	}
+
     [Route(template: "/api/cities/")]
     public class CitiesController : Controller
     {
@@ -17,7 +24,7 @@ namespace L22_ClassWork.Controllers
         public IActionResult GetCities()
         {
             List<City> cities = _dataStore.Cities;
-            
+
             if (cities == null)
                 return NotFound();
 
@@ -46,13 +53,53 @@ namespace L22_ClassWork.Controllers
                 return BadRequest();
 
             int newId = _dataStore.GetNewId();
-            var city = new City( newId, addCityModel.Name, addCityModel.Description, addCityModel.NumberOfPoi);
+            var city = new City(newId, addCityModel.Name, addCityModel.Description, addCityModel.NumberOfPoi);
 
             _dataStore.Cities.Add(city);
 
             var getCityModel = new GetCityModel(newId, addCityModel.Name, addCityModel.Description, addCityModel.NumberOfPoi);
 
-            return CreatedAtAction("GetCity", new { id = newId}, getCityModel);
+            return CreatedAtAction("GetCity", new { id = newId }, getCityModel);
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult UpdateCity(int id, Attribute attribute, string updatedAttribute)
+        {
+            City city = _dataStore.Cities.Where(c => c.Id == id).FirstOrDefault();
+
+            if (city == null)
+                return NotFound();
+
+            switch (attribute)
+            {
+                case Attribute.Name:
+                    if (updatedAttribute != city.Name && updatedAttribute != null)
+                        city.Name = updatedAttribute;
+                    break;
+
+                case Attribute.Description:
+                    if (updatedAttribute != city.Description && updatedAttribute != null)
+                        city.Name = updatedAttribute;
+                    break;
+
+                case Attribute.NumberOfPoi:
+                    try
+                    {
+                        int parsedAttribute = int.Parse(updatedAttribute);
+
+                        if (parsedAttribute != city.NumberOfPoi )
+                            city.NumberOfPoi = parsedAttribute;
+                        break;
+                    }
+                    catch
+					{
+                        return BadRequest("Can't parse to integer!");
+					}
+            }
+
+            var model = new GetCityModel(city.Id, city.Name, city.Description, city.NumberOfPoi);
+
+            return Ok();
         }
     }
 }
